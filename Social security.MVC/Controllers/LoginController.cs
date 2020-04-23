9d6ad04272dc8cb81327da0610557c5d6423095b
lenovo <lenovo@LAPTOP-9LFOOVPF>
2020-04-23 21:41:32 +0800

    登陆提交

--- Social security.MVC/Controllers/LoginController.cs
+++ Social security.MVC/Controllers/LoginController.cs
@@ -8,11 +8,40 @@ namespace Social_security.MVC.Controllers
 {
     public class LoginController : Controller
     {
-        //HttpClientHelper client = new HttpClientHelper("http://localhost:44319/");
+        HttpClientHelper client = new HttpClientHelper("http://localhost:44319/");
         // GET: Login
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public void Login(string name,string pwd ,string validCode)
+        {
+            var sessionValidCode = "";
+            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(pwd) && !string.IsNullOrEmpty(validCode))
+            {
+                sessionValidCode = Session["validCode"].ToString();
+                if (sessionValidCode.Equals(validCode.Trim(), StringComparison.CurrentCultureIgnoreCase))
+                {
+                    int i = int.Parse(client.Get("api/Social/Login?name=" + name + "&pwd=" + pwd));
+                    if (i>0)
+                    {
+                        Response.Write("<script>alert('登录成功！')</script>");
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('用户名或码错误！')</script>");
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('登录失败,验证码错误！')</script>");
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('账号、密码、验证码不能为空！')</script>");
+            }
         }
         public ActionResult CreateValidCodeImage()
         {
